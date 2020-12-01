463980dc8b35fa2e38096bb17dd8a7077d4f0259
John <john.darnall.fowler@gmail.com>
2020-11-30 20:00:14 -0500

    Initial Commit

---
+++ TheMovieManager/Controller/LoginViewController.swift
@@ -0,0 +1,63 @@
+//
+//  LoginViewController.swift
+//  TheMovieManager
+//
+//  Created by Owen LaRosa on 8/13/18.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var loginViaWebsiteButton: UIButton!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        emailTextField.text = ""
+        passwordTextField.text = ""
+    }
+    
+    //Once the login button is tapped, it gets a request Token using the getRequestToken method (it's completion handler passes a Bool if the parsing was sucessful), once the getRequestToken data session is complete, it's completion handler calls the handleRequestTokenResponse method, which takes two inputs: success and error, and simply prints the requestToken we downloaded
+    @IBAction func loginTapped(_ sender: UIButton) {
+        TMDBClient.getRequestToken(completion: handleRequestTokenResponse(success:error:))
+    }
+    
+    @IBAction func loginViaWebsiteTapped() {
+        performSegue(withIdentifier: "completeLogin", sender: nil)
+    }
+    
+    func handleRequestTokenResponse(success:Bool, error: Error?) {
+        if success {
+            //print(TMDBClient.Auth.requestToken)
+            //Required a reference to self for the variables below since they're global properties
+            DispatchQueue.main.async {
+                print("Calling Login Function")
+                TMDBClient.login(username: self.emailTextField.text ?? "", password: self.passwordTextField.text ?? "", completion: self.handleLoginResponse(success:Error:))
+            }
+        }
+    }
+    
+    func handleLoginResponse(success: Bool, Error: Error?) {
+        print("Handle Login Response Works")
+        if success {
+            print("Create Session ID is Called")
+            TMDBClient.createSessionId(completion: handleSessionResponse(success:Error:))
+        }
+    }
+    
+    func handleSessionResponse(success: Bool, Error: Error?) {
+        print("Handle Session Response Works")
+        if success {
+            DispatchQueue.main.async {
+                self.performSegue(withIdentifier: "completeLogin", sender: nil)
+            }
+        }
+        
+    }
+
+}
---
+++ TheMovieManager/Model/TMDB Client/TMDBClient.swift
@@ -0,0 +1,154 @@
+//
+//  TMDBClient.swift
+//  TheMovieManager
+//
+//  Created by Owen LaRosa on 8/13/18.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import Foundation
+
+class TMDBClient {
+    
+    static let apiKey = "7179587d60faf4f79d3952f873f04d8f"
+    
+    struct Auth {
+        static var accountId = 0
+        static var requestToken = ""
+        static var sessionId = ""
+    }
+    
+    enum Endpoints {
+        static let base = "https://api.themoviedb.org/3"
+        static let apiKeyParam = "?api_key=\(TMDBClient.apiKey)"
+     
+        
+        case getWatchlist
+        case getRequestToken
+        case login
+        case createSessionId
+        
+        var stringValue: String {
+            switch self {
+            case .getWatchlist: return Endpoints.base + "/account/\(Auth.accountId)/watchlist/movies" + Endpoints.apiKeyParam + "&session_id=\(Auth.sessionId)"
+            case .getRequestToken: return Endpoints.base + "/authentication/token/new" + Endpoints.apiKeyParam
+            case .login: return Endpoints.base + "/authentication/token/validate_with_login" + Endpoints.apiKeyParam
+            case .createSessionId: return Endpoints.base + "/authentication/session/new" + Endpoints.apiKeyParam
+            }
+        }
+        
+        var url: URL {
+            return URL(string: stringValue)!
+        }
+    }
+    
+    //Sends username and password, recieves a valid request token in return - returns True if the token is received
+    class func login(username: String, password: String, completion: @escaping (Bool, Error?) -> Void) {
+        //creates a URL Request that contains the information about the request (but isn't the actual request - the actual request is a URLSession - takes the link from the Endpoints struct and makes it a URL, then defines the different request components
+        print("Beginning of Login Function is Working")
+        var request = URLRequest(url: Endpoints.login.url)
+        request.httpMethod = "POST"
+        request.addValue("application/JSON", forHTTPHeaderField: "Content-Type")
+        //Defines the body of the request, which is a struct called Login Request, built according to the request body requirements in the API - the password and username will come from the text values entered into the view fields and stored in the LoginRequest struct. The requestToken was received with the getRequestToken function below - that's the only purpose for the function - go to that specifc URL and get a request token, which is the first step in the login process
+        let body = LoginRequest(userName: username, password: password, requestToken: Auth.requestToken)
+        print(Auth.requestToken + "Login Auth Working")
+        //Once the body is defined, it's encoded into a JSON object
+        request.httpBody = try! JSONEncoder().encode(body)
+        //After we have our JSON object, we can start a data session using the request. Swift should see the request compenents (httpMethod, addValue, body) and know what to do with it from there = once the request is sent, data is received back, which is checked for errors then decoded
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let data = data else {
+                completion(false, error)
+                print("No Data Recieved")
+                return
+            }
+            do {
+                let decoder = JSONDecoder()
+                print("Data is recieved by login function")
+                
+        //The data recieved from the request is decoded into an instance of the RequestTokenRepsonse struct
+        //The the Auth.requestToken property is updated from the data recieved from the request (we should have recieved a new request token that was validated by the site/user
+                print("Login Seems to Be Working Up to Decoding")
+                let responseObject = try decoder.decode(RequestTokenResponse.self, from: data)
+                
+                Auth.requestToken = responseObject.requestToken
+                print(Auth.requestToken + "Request Token")
+                
+                completion(true, nil)
+            }
+            catch {
+                completion(false, error)
+            }
+        }
+        //need to call this so we know the data task will be executed
+        task.resume()
+    }
+    
+    class func createSessionId(completion: @escaping (Bool, Error?) -> Void) {
+        
+        var request = URLRequest(url: Endpoints.createSessionId.url)
+        request.httpMethod = "POST"
+        request.addValue("application/JSON", forHTTPHeaderField: "Content-Type")
+        let body = PostSession(requestToken: Auth.requestToken)
+        //Once the body is defined, it's encoded into a JSON object
+        request.httpBody = try! JSONEncoder().encode(body)
+        //After we have our JSON object, we can start a data session using the request. Swift should see the request compenents (httpMethod, addValue, body) and know what to do with it from there = once the request is sent, data is received back, which is checked for errors then decoded
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let data = data else {
+                completion(false, error)
+                return
+            }
+            do {
+                let decoder = JSONDecoder()
+                //The data recieved from the request is decoded into an instance of the RequestTokenRepsonse struct
+                //The the Auth.requestToken property is updated from the data recieved from the request (we should have recieved a new request token that was validated by the site/user
+                let responseObject = try decoder.decode(SessionResponse.self, from: data)
+                Auth.sessionId = responseObject.sessionId
+                print(Auth.sessionId + "Auth Session ID Working")
+                completion(true, nil)
+                
+            }
+            catch {
+                completion(false, error)
+            }
+        }
+        //need to call this so we know the data task will be executed
+        task.resume()
+    }
+
+    
+    class func getWatchlist(completion: @escaping ([Movie], Error?) -> Void) {
+        let task = URLSession.shared.dataTask(with: Endpoints.getWatchlist.url) { data, response, error in
+            guard let data = data else {
+                completion([], error)
+                return
+            }
+            let decoder = JSONDecoder()
+            do {
+                let responseObject = try decoder.decode(MovieResults.self, from: data)
+                completion(responseObject.results, nil)
+            } catch {
+                completion([], error)
+            }
+        }
+        task.resume()
+    }
+    
+    class func getRequestToken(completion: @escaping (Bool, Error?) -> Void) {
+        let task = URLSession.shared.dataTask(with: Endpoints.getRequestToken.url) { data, response, error in
+            guard let data = data else {
+                completion(false, error)
+                return
+            }
+            let decoder = JSONDecoder()
+            do {
+                let responseObject = try decoder.decode(RequestTokenResponse.self, from: data)
+                completion(true, nil)
+                Auth.requestToken = responseObject.requestToken
+                print(Auth.requestToken + "getRequestToken")
+            } catch {
+                completion(false, error)
+            }
+        }
+        task.resume()
+    }
+}
